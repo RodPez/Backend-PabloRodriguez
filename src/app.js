545ec4db17b7fa0dfcc27429035803c920d4bf44
RodPez <pxblox@gmail.com>
2023-03-21 21:17:17 -0300

    Se agregaron views y se hicieron los correspondientes routeos

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 const ProductManager = require( "../class/ProductManager");
 const CartsManager = require("../class/CartsManager");
 const pathCarts = "./files/Carritos.json";
@@ -8,9 +9,15 @@ const listaDeCarts = new CartsManager(pathCarts);
 
 const productsRouter = require("./routes/products.router");
 const cartsRouter = require("./routes/carts.router")
-
+const indexRouter = require("./routes/index.router")
 
 const app = express();
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views")
+app.set("view engine", "handlebars")
+
+
 
 const port = 8080;
 
@@ -18,7 +25,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
       
 app.use("/api/products" ,productsRouter(listaDeProductos));
-app.use("/api/carts", cartsRouter(listaDeCarts,listaDeProductos))    
+app.use("/api/carts", cartsRouter(listaDeCarts,listaDeProductos));
+app.use("/index", indexRouter)    
 
 
 app.listen(port , () =>{
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -12,7 +12,8 @@ module.exports = (listaDeProductos) => {
             console.log(lista);
             const limit = parseInt(req.query.limit) || lista.length;
             const productosLimitados = lista.slice(0, limit);
-            res.status(200).json({productos: productosLimitados});   
+            //res.status(200).json({productos: productosLimitados});
+            res.status(200).render("productos.handlebars",{productos: productosLimitados, title: "Productos"})   
         } catch (error) {
             console.log(`Error al obtener la lista de productos: ${error}`);
             res.status(500).json({ error: "Error interno del servidor" });
