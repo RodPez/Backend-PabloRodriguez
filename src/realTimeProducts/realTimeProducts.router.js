e6f29c1ff3e2acd82aa15db78c9da20b10b0debc
RodPez <pxblox@gmail.com>
2023-03-24 20:57:22 -0300

    Corregi un temita de como generaba el id al cargar un nuevo producto en el realtimeproducts

--- src/realTimeProducts/realTimeProducts.router.js
+++ src/realTimeProducts/realTimeProducts.router.js
@@ -7,12 +7,12 @@ const router = Router();
 router.get("/", (req, res) =>{
     const productosRawData = fs.readFileSync(archivo);
     const productos = JSON.parse(productosRawData);
-    productosActuales = [...productos]
-    console.log(productosActuales);
+    productosActuales = [...productos];
     res.render("realTimeProducts.handlebars",{productos})
 })
 
 router.post("/", (req, res) =>{
+    const productosNuevos = [...productosActuales];
     const {title , description, price, thumbnail, code, stock} = req.body
     const newProductInfo ={
         title,
@@ -25,13 +25,13 @@ router.post("/", (req, res) =>{
     const codigoYaIngresado = productosActuales.filter(function(producto){
         return producto.code === newProductInfo.code
     })
-    const ultimoEnArray = productosActuales.length - 1
-    if (codigoYaIngresado.length === 0) {
-        const productosNuevos = [...productosActuales];
-        newProductInfo.id = productosActuales[ultimoEnArray].id + 1;
+   
+  
+    if (codigoYaIngresado.length === 0 || newProductInfo.code=== "") {
+        
+        newProductInfo.id = productosNuevos.length + 1;
         newProductInfo.status = true;
         productosNuevos.push(newProductInfo)
-        console.log(productosNuevos);
         const prodAJson = JSON.stringify(productosNuevos);
         console.log(prodAJson);
         fs.writeFileSync(archivo, prodAJson)
