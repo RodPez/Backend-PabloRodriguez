0ac0d4b01b2bc407349abd32b06f5b3bead5779b
RodPez <pxblox@gmail.com>
2023-05-06 14:15:12 -0300

    Se finalizo la implementacion de passport e inicio de sesion con github

--- src/auth/controller.auth.js
+++ src/auth/controller.auth.js
@@ -32,7 +32,10 @@ router.get("/github",passport.authenticate("github",{scope:["user: email"]}),
 )
 
 router.get("/githubcallback", passport.authenticate("github",{failureRedirect:"/login"}), 
-    async (req, res) =>{}
+    async (req, res) =>{
+        req.session.user = req.user;
+        res.redirect("/")
+    }
 )
 
 router.get("/logout", (req,res) =>{
